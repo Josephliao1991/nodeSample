5e92a39e447a798f35d0580549964ab3833dc214
Josephliao1991 <gn02239440@gmail.com>
2015-07-23 15:16:37 +0800

    no message

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var express = require('express');
-var bodyparser = require('body-parser');
-var methodOverride = require('method-override');
+// var bodyparser = require('body-parser');
+// var methodOverride = require('method-override');
 // var mongoose = require('mongoose');
 // var ip = '10.240.212.168'
 //
@@ -15,10 +15,10 @@ var methodOverride = require('method-override');
   var app = express();
 
 
-  app.use(bodyparser.json())
-  app.use(bodyparser.urlencoded({
-    extended: true
-  }))
+  // app.use(bodyparser.json())
+  // app.use(bodyparser.urlencoded({
+  //   extended: true
+  // }))
 
   app.get('/',function(request,response){
 
@@ -26,79 +26,81 @@ var methodOverride = require('method-override');
 
   });
 
-  app.get('/family',function(request, response){
-
-    // family.find(function(error, phone) {
-    //   // body...
-    //   if (error) {
-    //       response.send(error);
-    //   }
-    //
-    //   response.json(phone)
-    //
-    // })
-
-    response.end("Get family");
-
-  });
-
-  app.post('/family',function(request, response){
-
-    // family.create({
-    //     familyKey : request.body.familyKey,
-    //     deviceToken : request.body.deviceToken
-    // },function(error, family){
-    //   // body...
-    //   if (error) {
-    //       response.send(error)
-    //   }
-    //
-    //   family.find(function(error, family) {
-    //     // body...
-    //     if (error) {
-    //         response.send(error)
-    //     }
-    //
-    //     response.json(family)
-    //
-    //   })
-    // })
-    var familyKey = request.body.familyKey;
-    response.end("Post family : " + familyKey);
-
-  });
-
-  app.put('/family/:id',function(request, response){
-    // body...
-    var id = request.params.id
-    if (id == "abc123") {
-
-      var familyKey = request.body.familyKey;
-      response.end("Put family : " + familyKey);
-    }else {
-
-      response.end("Wrong id");
-    }
-  })
-
-  app.delete('/family/:id',function(request, response){
-    // body...
-    var id = request.params.id
-    if (id == "abc123") {
-      response.end("Delete family")
-    }else {
-      response.end("Wrong Id")
-    }
-
-  })
-
-  var server = app.listen(process.env.PORT||'8888','10.240.5.134',function(request, response) {
-    console.log('App listening at http://%s:%s', server.address().address, server.address().port);
-    console.log("Press Ctrl+C to quit.");
-  });
-
-  // app.listen(8888,'10.240.222.86',function(request, response) {
+  // app.get('/family',function(request, response){
+  //
+  //   // family.find(function(error, phone) {
+  //   //   // body...
+  //   //   if (error) {
+  //   //       response.send(error);
+  //   //   }
+  //   //
+  //   //   response.json(phone)
+  //   //
+  //   // })
+  //
+  //   response.end("Get family");
+  //
+  // });
+  //
+  // app.post('/family',function(request, response){
+  //
+  //   // family.create({
+  //   //     familyKey : request.body.familyKey,
+  //   //     deviceToken : request.body.deviceToken
+  //   // },function(error, family){
+  //   //   // body...
+  //   //   if (error) {
+  //   //       response.send(error)
+  //   //   }
+  //   //
+  //   //   family.find(function(error, family) {
+  //   //     // body...
+  //   //     if (error) {
+  //   //         response.send(error)
+  //   //     }
+  //   //
+  //   //     response.json(family)
+  //   //
+  //   //   })
+  //   // })
+  //   var familyKey = request.body.familyKey;
+  //   response.end("Post family : " + familyKey);
+  //
+  // });
+  //
+  // app.put('/family/:id',function(request, response){
   //   // body...
-  //   console.log("server start");
+  //   var id = request.params.id
+  //   if (id == "abc123") {
+  //
+  //     var familyKey = request.body.familyKey;
+  //     response.end("Put family : " + familyKey);
+  //   }else {
+  //
+  //     response.end("Wrong id");
+  //   }
+  // })
+  //
+  // app.delete('/family/:id',function(request, response){
+  //   // body...
+  //   var id = request.params.id
+  //   if (id == "abc123") {
+  //     response.end("Delete family")
+  //   }else {
+  //     response.end("Wrong Id")
+  //   }
   //
   // })
+
+  // var server = app.listen(process.env.PORT||'8888','10.240.5.134',function(request, response) {
+  //   console.log('App listening at http://%s:%s', server.address().address, server.address().port);
+  //   console.log("Press Ctrl+C to quit.");
+  // });
+
+  app.listen(8888,function(request, response) {
+    // body...
+    // console.log('App listening at http://%s:%s', server.address().address, server.address().port);
+    console.log("Press Ctrl+C to quit.");
+    console.log("server start");
+
+  })

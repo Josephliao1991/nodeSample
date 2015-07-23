16696ef2f15faa10b32a716cb481ffdd4ad65f1e
Josephliao1991 <gn02239440@gmail.com>
2015-07-23 16:27:13 +0800

    no message

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var methodOverride = require('method-override');
 var mongoose = require('mongoose');
 // var ip = '10.240.212.168'
 //
-mongoose.connect('mongodb:/10.240.250.86/home/powei_lin0906/db');
+mongoose.connect('mongodb:/10.240.250.86/my_datebase');
 
   // var family = mongoose.model('family', {
   //   familyKet : String,
@@ -95,7 +95,7 @@ mongoose.connect('mongodb:/10.240.250.86/home/powei_lin0906/db');
   //   console.log("Press Ctrl+C to quit.");
   // });
 
-  app.listen(8080,function(request, response) {
+  app.listen(8080,'10.240.102.112',function(request, response) {
     // body...
     // console.log('App listening at http://%s:%s', server.address().address, server.address().port);
     console.log("Press Ctrl+C to quit.");

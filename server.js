298b80cabf276cdfe8cfac2b4ff240e9d5e7a985
Josephliao1991 <gn02239440@gmail.com>
2015-07-24 14:23:19 +0800

    no message

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongoose.connect('mongodb://'+argv.be_ip+':80/my_database');
 
   var app = express();
 
-  app.use(connect.bodyparser());
+  app.use(bodyparser());
   app.use(bodyparser.json())
   app.use(bodyparser.urlencoded({extended: true}))
   app.use(bodyparser.json({ type: 'application/vnd.api+json' }))

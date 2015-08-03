f71527e11409d18e9d482cfba361df949bb1b6ac
Josephliao1991 <gn02239440@gmail.com>
2015-08-03 18:06:45 +0800

    no message

--- inedot.js
+++ inedot.js
@@ -37,25 +37,25 @@ function familyiNeDot(request, callback) {
 
   if (macAddr_find) {
 
-    iNeDot.find({familyKey : familyKey_find,
+    return iNeDot.find({familyKey : familyKey_find,
                  macAddr   : macAddr_find},
 
       function(error, inedots) {
         // body...
         if (error) {
-          return callback(error)
+         callback(error)
         }else {
-          return callback(null, inedots)
+         callback(null, inedots)
         }
       })
   }else {
-    iNeDot.find({familyKey : familyKey_find},
+    return iNeDot.find({familyKey : familyKey_find},
       function(error, inedots) {
         // body...
         if (error) {
-          return callback(error)
+         callback(error)
         }else {
-          return callback(null, inedots)
+         callback(null, inedots)
         }
       })
     }

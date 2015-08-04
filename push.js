ddb3b65855219c54d78bc1eca38202fec0395940
Josephliao1991 <gn02239440@gmail.com>
2015-08-04 15:16:33 +0800

    no message

--- push.js
+++ push.js
@@ -6,27 +6,16 @@ function sendPush(request, response) {
   // body...
   var familyKey_alert = request.body.familyKey
   var macAddr_alert   = request.body.macAddr
+  var xx;
+  xx = inedot.iNeDot.findOne({familyKey : familyKey_alert,
+                         macAddr   : macAddr_alert}).exec()
 
-  inedot.iNeDot.findOne({familyKey : familyKey_alert,
-                         macAddr   : macAddr_alert},
-    function(error,inedot) {
-      // body...
-      if (error) {
-        response.send(error)
-      }
-      if (inedot) {
-        var situation = inedot.situation
-        response.send(situation)
-      }else {
-        response.send("fail")
-      }
-
-    })
+  console.log(xx); 
 
     phone.Phone.findOne({familyKey  : familyKey_alert},
       function(error, phones) {
       // body...
-      response.send(phones);
+      console.log(phones);
 
     })
 

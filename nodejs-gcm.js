9647d66132a48eec6eb00f1c5e6053579dc81367
Josephliao1991 <gn02239440@gmail.com>
2015-08-07 10:36:25 +0800

    no message

--- nodejs-gcm.js
+++ nodejs-gcm.js
@@ -36,7 +36,7 @@ function sendAndroidPush(deviceToken,badgeNumber, situation, name, macAddr) {
   // create the message
   var msg = {
     // registration_ids: ['fy3VUb_Dorc:APA91bG08EPmMW1eKAKLB5jiae0epCFyQjo1PiPEKio9r0PNlo7r1T57DB9a96m693Z5NUqwiZJyCdTIH_MJwygfZD355TuUoqtpptf5SwQIcxmix7ZE4QW1dsHtnqhi6VPM0DuAA9pL'],
-    registration_ids: ['APA91bGDUiuEqcHck166Qh_yF9gXpzWlosMgfL9Q8lae1MLEb8ar0XdwrgJBDDKaXkxpWqEZf53SiGk7cQbri3dfoaOK76N2lk-9S1PabOn6JAc_dnAJwHvmfChOWu7V3af2GXEnJNcHSYau2H9BWucoZU7VcT62_Q'],
+    registration_ids: ['APA91bFK30b54oZVQF-cAu0rlNH8VPfiZHFv-ilHmpIrgXywtJ-VdJVQ-PcTv1UH097h9Q41a9NfvUkFu_ViuuiNuCZxU4QkTVan2mghxS5FgfwwwIxmrbJ5Ct4WPJ5Re_5aGUZA126u'],
     // registration_ids: [deviceToken],
     collapse_key: "0002dsaa", // http://developer.android.com/guide/google/gcm/gcm.html#send-msg
     time_to_live: 180, // just 30 minutes

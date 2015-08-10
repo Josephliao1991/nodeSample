8f198d4a03e15a3a8381558407dd4bcdd886a389
Josephliao1991 <gn02239440@gmail.com>
2015-08-10 15:46:45 +0800

    no message

--- nodejs-gcm.js
+++ nodejs-gcm.js
@@ -43,19 +43,22 @@ function sendAndroidPush(deviceToken,badgeNumber, situation, name, macAddr) {
     registration_ids: ['APA91bGDUiuEqcHck166Qh_yF9gXpzWlosMgfL9Q8lae1MLEb8ar0XdwrgJBDDKaXkxpWqEZf53SiGk7cQbri3dfoaOK76N2lk-9S1PabOn6JAc_dnAJwHvmfChOWu7V3af2GXEnJNcHSYau2H9BWucoZU7VcT62_Q','fy3VUb_Dorc:APA91bG08EPmMW1eKAKLB5jiae0epCFyQjo1PiPEKio9r0PNlo7r1T57DB9a96m693Z5NUqwiZJyCdTIH_MJwygfZD355TuUoqtpptf5SwQIcxmix7ZE4QW1dsHtnqhi6VPM0DuAA9pL'],
     collapse_key: "0002dsaa", // http://developer.android.com/guide/google/gcm/gcm.html#send-msg
     time_to_live: 180, // just 30 minutes
-    data: {
-      title       : "iNeDot",
-      message     : "Emergency From " + name, // your payload data
-      situation   : situation,
-      name        : name,
-      macAddr     : macAddr,
-      badgeNumber : badgeNumber
-    }
+    // data: {
+    //   title       : "iNeDot",
+    //   message     : "Emergency From " + name, // your payload data
+    //   situation   : situation,
+    //   name        : name,
+    //   macAddr     : macAddr,
+    //   badgeNumber : badgeNumber
+    // }
   };
 
   // send the message and see what happened
   gcm.send(msg, function(err, response) {
     // that error is from the http request, not gcm callback
+    if (error) {
+      console.log(error);
+    }
     console.log(response); // http://developer.android.com/guide/google/gcm/gcm.html#response
   });
 

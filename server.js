a688b791ef96699e1f1433d2999d511897839aac
Josephliao1991 <gn02239440@gmail.com>
2015-08-19 16:17:35 +0800

    no message

--- server.js
+++ server.js
@@ -312,8 +312,8 @@ var troubleHandler = require('./troubleHandler.js');
     var gyro     = req.body.gyro
 
 
-    console.log(acce.date);
-    console.log(gyro[0].xvalue);
+    console.log(acce[0][0]);
+    console.log(gyro[0]["xvalue"]);
     // for (var i = 0; i < acce.length; i++) {
     //   acce.push(req.body.acce[i]);
     //   console.log("AccE : "+req.body.acce[i]);

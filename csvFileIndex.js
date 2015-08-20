c9332e94734de4dc19acd05a936d9ddd47fead0d
Josephliao1991 <gn02239440@gmail.com>
2015-08-20 10:38:48 +0800

    no message

--- csvFileIndex.js
+++ csvFileIndex.js
@@ -34,12 +34,22 @@ function createFile(fileName) {
       console.log("fileExist Erroe: "+error);
     }
 
+    var fileName_accs = fileName+'_accs'
+    var fileName_gyro = fileName+'_gyro'
+
     if (!exist) {
-      csvFile.create({fileName : fileName},function (error,csvFile) {
+      csvFile.create({fileName : fileName_accs},function (error,csvFile) {
         // body...
         console.log("CSV FILE IS SAVE!!  ",+csvFile);
 
       })
+
+      csvFile.create({fileName : fileName_gyro},function (error,csvFile) {
+        // body...
+        console.log("CSV FILE IS SAVE!!  ",+csvFile);
+
+      })
+
     }else {
       console.log("CSV FILE NAME IS EXIST!");
     }

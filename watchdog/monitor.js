86e9282cf2ef21783d4f59e50c57000b0c42b1cf
Josephliao1991 <gn02239440@gmail.com>
2015-08-28 12:07:48 +0800

    no message

--- watchdog/monitor.js
+++ watchdog/monitor.js
@@ -2,7 +2,7 @@ var respawn = require('respawn');
 var util = require('util');
 var logger = require('./logger.js');
 
-var proc = respawn(['nodeSample', 'server.js'], {
+var proc = respawn(['inedot', 'server.js'], {
   cwd: '.',
   maxRestarts: 10,
   sleep: 1000,

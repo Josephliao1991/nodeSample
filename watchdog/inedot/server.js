9851564fc70320e9d651ffe25b7c3c6c0b456df2
Josephliao1991 <gn02239440@gmail.com>
2015-08-31 13:41:45 +0800

    no message

--- watchdog/inedot/server.js
+++ watchdog/inedot/server.js
@@ -516,7 +516,7 @@ var csvFile = require('./csv/csvFile.js');
 
   app.post('/array', function (request, response) {
     // body...
-    var array = request.body.array;
+    var array = JSON.stringifyrequest.body.array[0]);
     console.log(JSON.stringify(array));
     // console.log(array.alert.length);
     // console.log(array.alert);
